5bb9183b89bbb6483e6dfbb172b89ddf301b23bf
emanuel22gg <ospinae94@gmail.com>
2025-05-26 09:41:48 -0500

    se agregaron imagenes a los libros y cambie el tamaño

--- Proyecto/src/pag/props/libros.jsx
+++ Proyecto/src/pag/props/libros.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import LibrosInf from './librosinf'
+import libro1 from '../../assets/img/libroJs.png'
+import libro2 from '../../assets/img/libroSql.png'
+import libro3 from '../../assets/img/granLibro.png'
 const librosVent = [
     {
     libro: "programacion javascript",
     precio: 25000,
-    descripcion: "libro de programacion javascript"
+    descripcion: "introduccion a la programacion con javascript, fundamentos y conceptos basicos",
+    img: libro1
     },
     {
-    libro: "programacion java",
+    libro: "libro de sql",
     precio: 17000,
-    descripcion: "libro de programacion java"
+    descripcion: "habla sobre fundamentos basicos sobre bases de datos relacionales",
+    img: libro2
     },
+    {libro: "gran libro de html y css",
+    precio: 20000,
+    descripcion: "contiene informacion sobre html,css y javascript",
+    img: libro3
+    }
 ]
 
 export default function VentaLibros() {
     return (
-        librosVent.map(({libro, precio, descripcion}) => (
+        librosVent.map(({libro, precio, descripcion,img}) => (
             <LibrosInf
                 key={libro}
                 Nomlibro={libro}
                 precio={precio}
                 descripcion={descripcion}
+                img={img}
             />
         ))
     )
--- Proyecto/src/pag/props/librosinf.jsx
+++ Proyecto/src/pag/props/librosinf.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import BotonesC from "../estados/botonC";
 
-export default function LibrosInf({Nomlibro, precio, descripcion}) {
+
+export default function LibrosInf({Nomlibro, precio, descripcion, img}) {
     return (
-        <div className="card text-center shadow-sm" style={{ width: "18rem", height: "18rem", margin: "1rem", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.8)" }}>
+        <div className="card text-center shadow-sm" style={{ width: "19rem", height: "24rem", margin: "10px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.8)" }}>
             <div className="card-body d-flex flex-column justify-content-center align-items-center h-100">
-                <h5 className="card-title">{Nomlibro}</h5>
+                <h5 className="card-title"><strong>{Nomlibro}</strong></h5>
+                <img src={img} className="card-img-top" alt={Nomlibro} style={{ width: "100px", height: "100px", objectFit: "cover" }} />
                 <p className="card-text">
                     <strong>Precio: ${precio}</strong> 
                 </p>
